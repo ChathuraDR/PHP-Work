3d29b9c75fc8719a23dcd555ac22872a45ada6ad
Chathura D Ranathunga <Chathurad92@gmail.com>
2017-08-09 11:56:07 +0530

    Add files via upload

---
+++ phpFundamentals/GET Method/action.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ChathuraDR
+ * Date: 8/9/2017
+ * Time: 2:48 AM
+ */
+
+
+echo '<pre>';
+print_r($_GET);
+echo '<pre>';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP fundamentals</title>
+</head>
+<body>
+
+
+</body>
+</html>
+
---
+++ phpFundamentals/GET Method/index.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP fundamentals : Retrieving data From a database and add them into drop down list</title>
+    <style type="text/css">
+        /* ID Selector */
+        #test {
+            padding: 50px 0px 0px 0px;
+        }
+    </style>
+</head>
+<body>
+
+
+  <div id="test">
+      <form action="action.php" method="GET">
+          <fieldset>
+              <legend>Personal information:</legend>
+              First name:<br>
+              <input type="text" name="firstname">
+              <br>
+              Last name:<br>
+              <input type="text" name="lastname"><br>
+              <!-- In order for php to recognize more than 1 value in a checkbox scenario we have to add squre brackets in the name inside the HTML-->
+              <input type="checkbox" name="vehicle[]" value="Bike"> I have a bike
+              <input type="checkbox" name="vehicle[]" value="Car" checked="checked"> I have a car<br><br>
+              <input type="submit" value="Submit">
+          </fieldset>
+      </form>
+  </div>
+
+</body>
+</html>
